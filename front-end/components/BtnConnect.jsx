20e03a795fbd1fd00130cc16a86528c193bcfdd2
Brainfeed-1996 <olivierrobertduboille@gmail.com>
2024-01-05 14:34:04 +0100

    Olivier

---
+++ front-end/components/BtnConnect.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+function LoginButton() {
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (isSubmitting) {
+      alert(
+        "Vous ne pouvez pas envoyer plus d'une demande de connexion toutes les 10 secondes."
+      );
+      return;
+    }
+    setIsSubmitting(true);
+    setTimeout(() => {
+      setIsSubmitting(false);
+    }, 10000);
+    if (password === "monMotDePasse") {
+      setIsAdmin(true);
+      navigate("/admin");
+    } else if (password === "<PASSWORD>") {
+      setIsAdmin(false);
+      navigate("/user");}
+      else{
+      setErrorMessage("Mot de passe incorrect.");
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 10000);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="password">Mot de passe:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button variant="primary" type="submit">
+          Se connecter
+        </Button>
+      </div>
+      {errorMessage && <div>{errorMessage}</div>}
+      {isAdmin && (
+        <div style={{ color: "green" }}>
+          Vous êtes connecté en tant qu administrateur.
+        </div>
+      )}
+    </form>
+  );
+}
+export default LoginButton;
