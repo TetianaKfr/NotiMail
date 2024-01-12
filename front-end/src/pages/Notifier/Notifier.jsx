bbb99303fcee2c58b81bab1e145577373953bc0d
TetianaKfr <kosorukova.tetiana.a@gmail.com>
2024-01-12 11:27:51 +0100

    + notifier.css + Notifier.jsx

--- front-end/src/pages/Notifier/Notifier.jsx
+++ front-end/src/pages/Notifier/Notifier.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState,  useEffect } from 'react';
 import PropTypes from 'prop-types';
+import "../../pages/Notifier/notifier.css";
 
 
-const Modal = ({ show, handleClose }) => {
+const Modal = ({ show, handleClose, handleEnvoi }) => {
+  const [data, setData] = useState([]);
+  const [selectedCompany, setSelectedCompany] = useState("");
   const [companies, setCompanies] = useState([
     'Microsoft',
     'Google',
     'Meta',
     'Amazon',
     'Tesla Motors',
-    'Space X'
+    'Space X',
+    "Microsoft",
+    "Google",
+    "Meta",
+    "Amazon",
+    "Tesla Motors",
+    "Space X",
   ]);
-  const [selectedCompany, setSelectedCompany] = useState('');
+  
+  useEffect(() => {
+    fetch("/api/data")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
 
   const handleCompanyChange = (event) => {
     setSelectedCompany(event.target.value);
@@ -32,6 +46,30 @@ const Modal = ({ show, handleClose }) => {
         <button onClick={() => alert(`Vous avez sélectionné ${selectedCompany}`)}>
           Envoyer
         </button>
+        <div className="cadre">
+           <h2>Vous vous apprêtez à notifier :</h2>
+           <select
+             className="selected"
+             value={selectedCompany}
+             onChange={handleCompanyChange}
+             multiple
+           >
+             {companies.map((company) => (
+               <option key={company} value={company}>
+                 {company}
+               </option>
+             ))}
+           </select>
+           <div className="container">
+             <button className="close" onClick={handleClose}>
+               Annuler
+             </button>
+             <button className="envoi" onClick={handleEnvoi}>
+               Envoyer
+             </button>
+          </div>
+      </div>
+
       </section>
     </div>
   );
@@ -44,8 +82,11 @@ const MyComponent = ({ show }) => {
   );
 };
 
-MyComponent.propTypes = {
-  show: PropTypes.bool.isRequired,
-};
+
+  Modal.propTypes = {
+    show: PropTypes.bool.isRequired,
+    handleClose: PropTypes.func.isRequired,
+    handleEnvoi: PropTypes.func.isRequired,
+  };
 
 export default Modal;
