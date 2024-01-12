3d87832794e6f718f17c919c0bd5208bf06cc90b
Brainfeed-1996 <olivierrobertduboille@gmail.com>
2024-01-12 10:12:07 +0100

    Modifs*

--- front-end/src/pages/Notifier/Notifier.jsx
+++ front-end/src/pages/Notifier/Notifier.jsx
@@ -1,51 +1,64 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "../../pages/Notifier/notifier.css";
 
-
-const Modal = ({ show, handleClose }) => {
+const Modal = ({ show, handleClose, handleEnvoi }) => {
+  const [data, setData] = useState([]);
+  const [selectedCompany, setSelectedCompany] = useState("");
   const [companies, setCompanies] = useState([
-    'Microsoft',
-    'Google',
-    'Meta',
-    'Amazon',
-    'Tesla Motors',
-    'Space X'
+    "Microsoft",
+    "Google",
+    "Meta",
+    "Amazon",
+    "Tesla Motors",
+    "Space X",
   ]);
-  const [selectedCompany, setSelectedCompany] = useState('');
+
+  useEffect(() => {
+    fetch("/api/data")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
 
   const handleCompanyChange = (event) => {
     setSelectedCompany(event.target.value);
   };
 
   return (
-    <div className={show ? 'modal display-block' : 'modal display-none'}>
+    <div className={show ? "modal display-block" : "modal display-none"}>
       <section className="modal-main">
-        <h2>Vous vous apprêtez à notifier :</h2>
-        <select value={selectedCompany} onChange={handleCompanyChange}>
-          {companies.map((company) => (
-            <option key={company} value={company}>
-              {company}
-            </option>
-          ))}
-        </select>
-        <button onClick={handleClose}>Annuler</button>
-        <button onClick={() => alert(`Vous avez sélectionné ${selectedCompany}`)}>
-          Envoyer
-        </button>
+        <div className="cadre">
+          <h2>Vous vous apprêtez à notifier :</h2>
+          <select
+            className="selected"
+            value={selectedCompany}
+            onChange={handleCompanyChange}
+            multiple
+          >
+            {companies.map((company) => (
+              <option key={company} value={company}>
+                {company}
+              </option>
+            ))}
+          </select>
+          <div className="container">
+            <button className="close" onClick={handleClose}>
+              Annuler
+            </button>
+            <button className="envoi" onClick={handleEnvoi}>
+              Envoyer
+            </button>
+          </div>
+        </div>
       </section>
     </div>
   );
 };
-const MyComponent = ({ show }) => {
-  return (
-    <div>
-      {show ? <p>Le composant est affiché</p> : <p>Le composant est masqué</p>}
-    </div>
-  );
-};
 
-MyComponent.propTypes = {
+Modal.propTypes = {
   show: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  handleEnvoi: PropTypes.func.isRequired,
 };
 
 export default Modal;
