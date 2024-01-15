b813b71a06711af0e60baf59e58b9c64d1af4b54
Brainfeed-1996 <olivierrobertduboille@gmail.com>
2024-01-15 11:04:49 +0100

    modifications

--- front-end/src/pages/panneauAdmin/PanneauAdmin.jsx
+++ front-end/src/pages/panneauAdmin/PanneauAdmin.jsx
@@ -1,17 +1,17 @@
-import {useState} from 'react'; //Importe le hook useState
-import "./panneauAdmin.css"
-import { FaSearch } from 'react-icons/fa';
+import { useState } from "react"; //Importe le hook useState
+import "./panneauAdmin.css";
+import { FaSearch } from "react-icons/fa";
 import { IoMdAddCircle } from "react-icons/io";
 import { BiMailSend } from "react-icons/bi";
-import { NavLink } from 'react-router-dom';
-import ModalNotifier from '../../pages/Notifier/Notifier.jsx';
+import { NavLink } from "react-router-dom";
+import ModalNotifier from "../../pages/Notifier/Notifier.jsx";
 
-  const PanneauAdmin  = ({ handleBiMailSendClick }) => {
-    const [showModal, setShowModal] = useState(true);
-  
-    const handleCloseModal = () => {
-      setShowModal(false);
-    };
+const PanneauAdmin = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
   const [isChecked, setIsChecked] = useState(false);
 
@@ -23,24 +23,27 @@ import ModalNotifier from '../../pages/Notifier/Notifier.jsx';
     setIsChecked(event.target.checked);
   };
 
-
   return (
     <>
-    <div className="bandeau">
-      <div className="search-bar">
-        <FaSearch className="search-icon" />
-        <input type="text" placeholder="Rechercher..." />
+      <div className="bandeau">
+        <div className="search-bar">
+          <FaSearch className="search-icon" />
+          <input type="text" placeholder="Rechercher..." />
         </div>
-    </div>
+      </div>
       <div className="cards">
         <div className="card" id="Entreprise1">
-        <div className="c-toggleicon02">
-          {/* Input de type checkbox
+          <div className="c-toggleicon02">
+            {/* Input de type checkbox
             'checked' lie l'état de la checkbox à l'état 'isChecked'
             'onChange' déclenche 'handleCheckboxChange' lors d'un clic sur la checkbox */}
-            <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
             <span className="icon"></span>
-        </div>
+          </div>
           <h3>Entreprise 1</h3>
           <ul className="inline-items">
             <li>Nom</li>
@@ -49,10 +52,14 @@ import ModalNotifier from '../../pages/Notifier/Notifier.jsx';
           </ul>
         </div>
         <div className="card" id="Entreprise2">
-        <div className="c-toggleicon02">
-            <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+          <div className="c-toggleicon02">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
             <span className="icon"></span>
-        </div>
+          </div>
           <h3>Entreprise 2</h3>
           <ul className="inline-items">
             <li>Nom</li>
@@ -61,10 +68,14 @@ import ModalNotifier from '../../pages/Notifier/Notifier.jsx';
           </ul>
         </div>
         <div className="card" id="Entreprise3">
-        <div className="c-toggleicon02">
-            <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+          <div className="c-toggleicon02">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
             <span className="icon"></span>
-        </div>
+          </div>
           <h3>Entreprise 3</h3>
           <ul className="inline-items">
             <li>Nom</li>
@@ -73,10 +84,14 @@ import ModalNotifier from '../../pages/Notifier/Notifier.jsx';
           </ul>
         </div>
         <div className="card" id="Entreprise4">
-        <div className="c-toggleicon02">
-            <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+          <div className="c-toggleicon02">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
             <span className="icon"></span>
-        </div>
+          </div>
           <h3>Entreprise 4</h3>
           <ul className="inline-items">
             <li>Nom</li>
@@ -85,10 +100,14 @@ import ModalNotifier from '../../pages/Notifier/Notifier.jsx';
           </ul>
         </div>
         <div className="card" id="Entreprise5">
-        <div className="c-toggleicon02">
-            <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+          <div className="c-toggleicon02">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
             <span className="icon"></span>
-        </div>
+          </div>
           <h3>Entreprise 5</h3>
           <ul className="inline-items">
             <li>Nom</li>
@@ -96,19 +115,29 @@ import ModalNotifier from '../../pages/Notifier/Notifier.jsx';
             <li>Date</li>
           </ul>
         </div>
-
       </div>
-      <footer >
+      <footer>
         <div className="logos-footer">
           <NavLink to="/entreprises">
             <IoMdAddCircle className="icon-style" />
           </NavLink>
           {/*<button className="icon-style" onClick={handleBiMailSendClick}></button>*/}
-          <BiMailSend className="icon-style" onclick={handleBiMailSendClick}/>
-          <ModalNotifier show={showModal} handleClose={handleCloseModal} handleEnvoi={() => {}} />
+          <BiMailSend
+            className="icon-style"
+            onClick={() => {
+              setShowModal(!showModal);
+            }}
+          />
+
+          {showModal && (
+            <ModalNotifier
+            /* show={showModal}
+              handleClose={handleCloseModal}
+              handleEnvoi={() => {}} */
+            />
+          )}
         </div>
       </footer>
-      {showModal && <ModalNotifier />}
     </>
   );
 };
