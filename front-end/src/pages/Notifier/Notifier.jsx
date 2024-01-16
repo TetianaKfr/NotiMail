f235610fcde98687ed1ceab7715a9f3f3958fc70
TetianaKfr <kosorukova.tetiana.a@gmail.com>
2024-01-16 15:44:08 +0100

    comments to Notifier.jsx

--- front-end/src/pages/Notifier/Notifier.jsx
+++ front-end/src/pages/Notifier/Notifier.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "../../pages/Notifier/notifier.css";
 
+// Composant Modal
 const Modal = ({ show, handleClose, handleEnvoi }) => {
-  const [data, setData] = useState([]);
-  const [selectedCompany, setSelectedCompany] = useState("");
-  const [companies, setCompanies] = useState([
+  // État local
+  const [data, setData] = useState([]); // Données récupérées de l'API
+  const [selectedCompany, setSelectedCompany] = useState(""); // Entreprise sélectionnée dans la liste déroulante
+  const [companies, setCompanies] = useState([ // Liste des entreprises
     "Microsoft",
     "Google",
     "Meta",
@@ -20,16 +22,17 @@ const Modal = ({ show, handleClose, handleEnvoi }) => {
     "Space X",
   ]);
 
+  // Effet secondaire pour récupérer des données de l'API lors du montage
   useEffect(() => {
     fetch("/api/data")
       .then((response) => response.json())
       .then((data) => setData(data));
   }, []);
-
+   // Fonction de gestion du changement d'entreprise dans la liste déroulante
   const handleCompanyChange = (event) => {
     setSelectedCompany(event.target.value);
   };
-
+  // Rendu JSX du composant Modal
   return (
     <div>
       <section className="modal-main">
@@ -60,18 +63,20 @@ const Modal = ({ show, handleClose, handleEnvoi }) => {
     </div>
   );
 };
+// Composant MyComponent
 const MyComponent = ({ show }) => {
+  // Rendu JSX du composant MyComponent
   return (
     <div>
       {show ? <p>Le composant est affiché</p> : <p>Le composant est masqué</p>}
     </div>
   );
 };
-
+// Propriétés PropTypes pour le composant Modal
 Modal.propTypes = {
   show: PropTypes.bool.isRequired,
   handleClose: PropTypes.func.isRequired,
   handleEnvoi: PropTypes.func.isRequired,
 };
-
+// Export du composant Modal
 export default Modal;
