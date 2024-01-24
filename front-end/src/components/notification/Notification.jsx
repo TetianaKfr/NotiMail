a4728425c17315d6fb69614f0d117d76a5335ba6
malikamezaoui <84177863+malikamezaoui@users.noreply.github.com>
2024-01-24 13:38:26 +0100

    modification notification.jsx

--- front-end/src/components/notification/Notification.jsx
+++ front-end/src/components/notification/Notification.jsx
@@ -1,4 +1,6 @@
-
+import Valid from '../../assets/images/vector-valid.svg'
+import Cross from '../../assets/images/vector-cross.svg'
+import MailRedDot from '../../assets/images/mail-reddot.svg'
 import React, { useState, useRef } from 'react';
 import './notification.css';
 
@@ -6,66 +8,57 @@ import './notification.css';
 // Il contient deux boutons - un pour confirmer et un autre pour annuler.
 const Modal = (props) => {
 
-    const dialog = useRef(null);
-  
-    const openHandler = () => {
-      dialog.current.showModal();
-    }
-  
-    const closeHandler = () => {
-      dialog.current.close();
-    }
+  const dialog = useRef(null);
 
-    return (
-        <>
-        <div className="modal-overlay">
-        
+  const openHandler = () => {
+    dialog.current.showModal();
+  }
+
+  const closeHandler = () => {
+    dialog.current.close();
+  }
+
+  return (
+    <>
+      <div className="modal-overlay">
+
         <dialog ref={dialog}>
-        <p>Confirmer la réception du courrier</p>
-        <button onClick={() => { props.cancel(false) }}>annuler</button>
-                    <button onClick={() => { props.validate(true) }}>valider</button>
-      </dialog>
-      <button type="button" onClick={openHandler}>
-        Réceptionner
-      </button>
+          <p>Confirmer la réception du courrier</p>
+          <button onClick={() => { closeHandler() }}><img src={Cross}/></button>
+          <button onClick={() => { closeHandler(); props.validate() }}><img src={Valid}/></button>
+        </dialog>
+        <button className="receptionner" type="button" onClick={openHandler}>
+          Réceptionner
+        </button>
       </div>
-        </>
-    );
+    </>
+  );
 }
 
 // Composant Notification : Gère l'affichage du Modal et le bouton de confirmation.
 const Notification = () => {
-    // State isOpen pour contrôler l'affichage du Modal
-    const [isOpen, setIOpen] = useState(false);
 
-    // Fonction pour gérer l'action de confirmation
-    const onConfirm = () => {
-        setIOpen(true);
-        console.log('Confirmation effectuée');
-    };
 
-    const onCancel = () => {
-        setIOpen(false);
-    }
+  const onValidate = () => {
+    alert("Courrier reçu");
+  }
 
-    const onValidate = () => {
-        setIOpen(false);
-        alert("Courrier reçu");
-    }
+  return (
+    <>
+      <div className='alert'>
+        <div className='image-mail-attente'>
+          <img src={MailRedDot} alt="e-mail en attente" />
+        </div>
+        <div className="string-container">
+          {/* Bouton pour déclencher la confirmation et afficher le Modal */}
+          <span >Vous avez du courrier en attente</span>
+        </div>
 
-    return (
-        <>
-            <div className='alert'>
-                <div className="button-container">
-                    {/* Bouton pour déclencher la confirmation et afficher le Modal */}
-                    <span onClick={onConfirm}>Vous avez du courrier en attente</span>
-                </div>
-            
-            {/* Affiche le Modal si isOpen est vrai */}
-            {isOpen && <Modal cancel={onCancel} validate={onValidate} />}
-            </div>
-        </>
-    );
+        {/* Affiche le Modal si isOpen est vrai */}
+        <Modal validate={onValidate} />
+      </div>
+    </>
+  );
 }
 
 export default Notification;
